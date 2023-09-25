b4921f8d245d274f727c16a781afe212168d4eec
Cinthia <cinthiasanchezr@1997@gmail.com>
2023-09-25 09:32:07 -0700

    create login part1

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Navbar from "./components/Navbar/Navbar";
 import Example from "./components/Navbar/Example";
 import MiniDrawer from "./components/Navbar/Menu";
+import RegisterForm from "./components/Login/Register";
+
 export default function Page() {
   return (
     <>
       <MiniDrawer />
-      {/* <Navbar /> */}
+      {/* <RegisterForm /> */}
     </>
     // <h1>Rama Dev!</h1>;
   );
